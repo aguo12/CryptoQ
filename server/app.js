3bbd3d861688ee5c0bfdf1bdf9a0f0bc5ca6575b
Andy <info@aguo.dev>
2021-02-14 04:35:37 -0500

    feat: more stuiff

--- server/app.js
+++ server/app.js
@@ -11,11 +11,23 @@ let config = JSON.parse(fs.readFileSync('./config.json'))
 
 app.get('*', (req, res) => {
   let reqPath = req.path.replace('/', '')
-  if (config.events[reqPath] != null) {
-    if (Object.getOwnPropertyNames(req.cookies).length != 0) {
-      res.sendFile(path.resolve('../client/content.html'));
-      // Disassemble cookie and match sig, then allow if valid
+  if (config.events[reqPath] != null && config.events[reqPath]['isActive'] == true) { // Checks if the event name exists & that the event is active
+    if (Object.getOwnPropertyNames(req.cookies).length != 0 && req.cookies['queueToken'] != null) { // Checks if the request includes cookies and if it includes queue cookie
+      let queueToken = req.cookies['queueToken'];
+      verifyToken(queueToken).then(status => {
+        switch(status){
+          case "allowed":
+            res.sendFile(path.resolve('../client/content.html'));
+            break;
+          case "wait":
+            res.sendFile(path.resolve('../client/queue.html'));
+            break;
+          case "expired":
+            break;
+        }
+      })
     } else {
+      createCookie(req.ip)
       res.sendFile(path.resolve('../client/queue.html'));
     }
   } else {
