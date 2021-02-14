9807157dce18e8b8ce90315111026a4ab0df5f93
Andy <info@aguo.dev>
2021-02-14 03:10:51 -0500

    feat: queue page

---
+++ client/script.js
@@ -0,0 +1,7 @@
+window.onload = (event) => {
+    console.log('Initiating browser detection sequences.');
+  };
+
+  // Analyze browser data. Aggregate by using encryption methods. 
+  // Compose data into JSON, and send it to server.
+  // Validate data server-side, and send back encrypted cookie to track/verify user.
--- server/app.js
+++ server/app.js
@@ -1,21 +1,28 @@
 const express = require('express')
+var cookieParser = require('cookie-parser')
 const app = express()
+app.use(cookieParser())
 const port = 3000
-
-let requestRouter = (req, res, next) => {
-    console.log('LOGGED')
-    next()
-}
+const path = require('path')
 
 app.get('/content', (req, res) => {
-  res.send('Hello World!')
+  if(Object.getOwnPropertyNames(req.cookies).length != 0){
+      res.send("Welcome to the content! You made it!")
+      // Disassemble cookie and match sig, then allow if valid
+  } else {
+      res.sendFile(path.resolve('../client/queue.html'));
+  }
 })
-
-app
 
 app.listen(port, () => {
   console.log(`Demo server initialized!`)
 })
 
-app.use(requestRouter)
+let processCookie = new Promise((resolve,reject) => {
+  // Decrypt cookie and sign a new one
+})
+
+let createCookie = new Promise((resolve, reject) => {
+  
+})
 
