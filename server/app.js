a8309eabbd2ffd756942df18b1c0a0d1d7e5daf9
Andy <info@aguo.dev>
2021-02-14 04:12:12 -0500

    feat: more stuff

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const CryptoJS = require("crypto-js");
 const fs = require('fs')
 const path = require('path')
 var cookieParser = require('cookie-parser')
@@ -12,7 +13,7 @@ app.get('*', (req, res) => {
   let reqPath = req.path.replace('/', '')
   if (config.events[reqPath] != null) {
     if (Object.getOwnPropertyNames(req.cookies).length != 0) {
-      res.send("Welcome to the content! You made it!")
+      res.sendFile(path.resolve('../client/content.html'));
       // Disassemble cookie and match sig, then allow if valid
     } else {
       res.sendFile(path.resolve('../client/queue.html'));
@@ -26,10 +27,18 @@ app.listen(port, () => {
   console.log(`Demo server initialized!`)
 })
 
-let processCookie = new Promise((resolve, reject) => {
-  // Decrypt cookie and sign a new one
-})
+let processCookie = () => {
+  return new Promise((resolve, reject) => {
+    // Decrypt cookie and sign a new one
+  })
+}
 
-let createCookie = new Promise((resolve, reject) => {
-
-})
+let encryptString = (cookieString, keyId) => {
+  return new Promise((resolve, reject) => {
+    if(config.events[keyId] != null){
+        console.log(CryptoJS.HmacSHA256(cookieString, "Key"));
+    } else{
+        resolve(false)
+    }
+  })
+}
