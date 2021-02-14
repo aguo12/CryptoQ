cff070fb60e0097c11865553d6894b68c4270cc2
Andy <info@aguo.dev>
2021-02-14 03:40:25 -0500

    feat: added 404 page

--- server/app.js
+++ server/app.js
@@ -1,16 +1,24 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 var cookieParser = require('cookie-parser')
 const app = express()
 app.use(cookieParser())
-const port = 3000
-const path = require('path')
 
-app.get('/content', (req, res) => {
-  if(Object.getOwnPropertyNames(req.cookies).length != 0){
+const port = 3000
+let config = JSON.parse(fs.readFileSync('./config.json'))
+
+app.get('*', (req, res) => {
+  let reqPath = req.path.replace('/', '')
+  if (config.events[reqPath] != null) {
+    if (Object.getOwnPropertyNames(req.cookies).length != 0) {
       res.send("Welcome to the content! You made it!")
       // Disassemble cookie and match sig, then allow if valid
-  } else {
+    } else {
       res.sendFile(path.resolve('../client/queue.html'));
+    }
+  } else {
+    res.sendFile(path.resolve('../client/404.html'));
   }
 })
 
@@ -18,11 +26,10 @@ app.listen(port, () => {
   console.log(`Demo server initialized!`)
 })
 
-let processCookie = new Promise((resolve,reject) => {
+let processCookie = new Promise((resolve, reject) => {
   // Decrypt cookie and sign a new one
 })
 
 let createCookie = new Promise((resolve, reject) => {
-  
-})
 
+})
