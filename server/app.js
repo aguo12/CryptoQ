edb04d0ae1f777a8348cf03f4386098ee27c9a33
Andy Guo <info@aguo.dev>
2021-03-28 00:01:15 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 const path = require('path')
 const cookieParser = require('cookie-parser');
 const qs = require('qs');
-4
 const app = express()
 app.use(cookieParser())
 app.set('trust proxy', true)
